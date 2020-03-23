0781cbdeac8fe4d3ba8d414745911da4a02bf649
wgywmm <13502960@qq.com>
2020-03-23 23:12:26 +0800

    edit zh-CN

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,16 @@
 module.exports = {
+  title: '三国演义',
+  description: '《三国演义》是中国文学史上第一部章回小说，是历史演义小说的开山之作，也是第一部文人长篇小说，中国古典四大名著之一。',
+  evergreen: true,
   themeConfig: {
+    nav: [
+      {
+        text: '语言',
+        items: [
+          { text: '简体中文', link: '/zh-CN/' },
+        ]
+      }
+    ],
     sidebar: {
       '/zh-CN/': getzhcnSide()
     }
